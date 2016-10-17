777dba9e1de79d7ec5af62abc3957d75b7158db6
donovandesmedt <donovandesmedt@live.be>
2016-10-17 17:14:07 +0200

    Initial commit

--- OpendeurdagApp/MainPage.xaml.cs
+++ OpendeurdagApp/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using OpendeurdagApp.view;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +26,21 @@ namespace OpendeurdagApp
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void Hoofdmenu_OnClick(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private void OpleidingenOverzicht_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            mainFrame.Navigate(typeof (opleidingen));
+        }
+
+        private void Opendeur_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
