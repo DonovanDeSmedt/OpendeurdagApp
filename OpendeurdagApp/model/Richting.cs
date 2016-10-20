1e16586cf641a46d9d4728ceb799b22e6a94f92a
donovandesmedt <donovandesmedt@live.be>
2016-10-20 21:25:33 +0200

    Binding data CampussenView

---
+++ OpendeurdagApp/model/Richting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpendeurdagApp.model
+{
+    class Richting
+    {
+        public string Naam { get; set; }
+    }
+}
--- OpendeurdagApp/view/campussen.xaml.cs
+++ OpendeurdagApp/view/campussen.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using OpendeurdagApp.model;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,9 +23,27 @@ namespace OpendeurdagApp.view
     /// </summary>
     public sealed partial class campussen : Page
     {
+        private static List<Richting> listRichtingen = new List<Richting>{new Richting() {Naam = "Toegepaste informatica"}, new Richting() { Naam = "Bedrijfsmanagement" } };
+        
+        private static Campus schoonmeersen = new Campus()
+        {
+            Adres = "Valentin Vaerweglaan 9000 Gent",
+            Naam = "Campus Schoonmeersen",
+            Richtingen = listRichtingen
+        };
+        private static Campus ledeganck = new Campus()
+        {
+            Adres = "Centrum 9000 Gent",
+            Naam = "Campus Ledeganck",
+            Richtingen = listRichtingen
+        };
+        private List<Campus> listCampussen = new List<Campus> { schoonmeersen, ledeganck };
+        
+
         public campussen()
         {
             this.InitializeComponent();
+            this.DataContext = schoonmeersen;
         }
         private void Richting_OnTapped(object sender, TappedRoutedEventArgs e)
         {
